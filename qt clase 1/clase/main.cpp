8ee52485277165223178a0a3a4b25a78c96be94c
Julián Sanchez <julian.sanchez6@udea.edu.co>
2020-09-10 09:21:33 -0500

    actualización 1

--- qt clase 1/clase/main.cpp
+++ qt clase 1/clase/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
 using namespace std;
-
+int contador = 0;
 int main()
 {
-    cout << "Hello World!" << endl;
+    for(int i = 0; i < 100; i +=4){
+        cout << 'iteracion' << contador ++<< i << endl;
+    }
+    system("PAUSE");
     return 0;
 }
